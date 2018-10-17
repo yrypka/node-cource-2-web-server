5b6c8ed763bcc7b6403c04b9de4265e438f77cff
Yuriy Rypka <yurarypka@gmail.com>
2018-10-17 20:37:01 +0300

    rendering templates with handlebars (hbs npm package)
    Alternatives to hbs are pug, ejs.

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
+app.set('view engine', 'hbs')
 app.use(express.static(`${__dirname}/public`))
 
+const currentYear = new Date().getFullYear()
+
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>')
-  res.send({
-    name: 'Andrew',
-    likes: ['Biking', 'Cities']
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website',
+    currentYear
   })
 })
 
 app.get('/about', (req, res) => {
-  res.send('About Page')
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear
+  })
 })
 
 app.get('/bad', (req, res) => {
