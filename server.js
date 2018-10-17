5b1d461e63fc4d6c5b790bd636be2c3a70bde01d
Yuriy Rypka <yurarypka@gmail.com>
2018-10-17 20:55:37 +0300

    advanced templating

--- server.js
+++ server.js
@@ -3,23 +3,24 @@ const hbs = require('hbs')
 
 const app = express()
 
+hbs.registerPartials(`${__dirname}/views/partials`)
 app.set('view engine', 'hbs')
 app.use(express.static(`${__dirname}/public`))
 
-const currentYear = new Date().getFullYear()
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear())
+
+hbs.registerHelper('screamIt', text => text.toUpperCase())
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website',
-    currentYear
+    welcomeMessage: 'Welcome to my website'
   })
 })
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear
+    pageTitle: 'About Page'
   })
 })
 
